72da9ba32dc2c313d53f519728ba5218b7787a9c
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-07-31 16:30:30 +0530

    add compact upload

--- src/components/file-upload/compact-upload.tsx
+++ src/components/file-upload/compact-upload.tsx
@@ -1,12 +1,28 @@
+import { cn } from "@/lib/utils";
 import { UploadConfig } from "./types";
+import { fileUploadVariants } from "./variants";
+import { ButtonUpload } from "./button-upload";
+import { MdAdd } from "react-icons/md";
 
-export function CompactUpload({ config, ref, validateFile, setFiles }: { config: UploadConfig, ref: React.RefObject<HTMLInputElement | null>, validateFile: (fileList: FileList) => File[], setFiles: (files: File[]) => void }) {
+interface CompactUploadProps {
+    config: UploadConfig;
+    ref: React.RefObject<HTMLInputElement | null>;
+}
+
+export function CompactUpload({ config, ref }: CompactUploadProps) {
     const { variant, size, label, theme } = config;
     const fileInputRef = ref;
 
     return (
-        <div>
-            <h1>Compact Upload</h1>
+        <div className={cn(fileUploadVariants({ variant, size, bgTheme: theme?.bgTheme, radius: theme?.radius, borderStyle: theme?.borderStyle}))}>
+            {label.button && (
+                <p className={cn(size === "sm" ? "text-xs" : size === "md" ? "text-sm" : "text-base")}>
+                    {label.button} 
+                </p>
+            )}
+            <ButtonUpload config={{ ...config, variant: "button", size: "sm" }} ref={fileInputRef} >
+                <MdAdd className={cn("w-4 h-4")} />
+            </ButtonUpload>
         </div>
     )
 }
