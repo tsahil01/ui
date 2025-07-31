32ae5f400e50db971dfdd7eda22eff2df82660c2
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-07-31 17:00:48 +0530

    exports

---
+++ src/components/file-upload/index.ts
@@ -0,0 +1,8 @@
+export { default as FileUpload } from "./file-upload";
+export type { UploadConfig } from "./types";
+export { ButtonUpload } from "./button-upload";
+export { DragDropUpload } from "./drag-drop-upload";
+export { PreviewUpload } from "./preview-upload";
+export { CompactUpload } from "./compact-upload";
+export { default as FileList } from "./file-list";
+export { FilePreviewDialog } from "./preview-upload";
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { FileUpload } from "./components/file-upload";
+export type { UploadConfig } from "./components/file-upload/types";
