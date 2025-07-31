c3c1e3920a962e64a03cfcc341b902bada80dff4
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-07-31 19:46:30 +0530

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,24 @@
-"use client";
-
-import { cn } from "@/lib/utils";
-import { FileUpload, UploadConfig } from "@tsahil01/file-upload";
+import Link from "next/link";
 
 export default function Home() {
-  const config: UploadConfig = {
-    variant: "dragDrop",
-    size: "md",
-    allowMultiple: true
-    ,
-    maxSizeInMb: 10,
-    accept: ["image/*", "application/pdf"],
-    label: {
-      button: "Upload Documents",
-      dropZone: "Drag and drop files here.",
-    },
-    theme: {
-      bgTheme: "dark",
-      radius: "lg",
-      borderStyle: "double",
-    },
-  }
 
-  const handleUpload = () => {
-    console.log("Uploading...");
-  }
 
   return (
-    <div className={cn("flex items-center justify-center h-screen w-screen mx-auto p-4", {
-      "bg-zinc-100": config.theme?.bgTheme === "light",
-      "bg-zinc-950": config.theme?.bgTheme === "dark",
-    })}>
-      <FileUpload config={config} onUpload={handleUpload} />
+    <div className={"flex items-center h-screen w-screen mx-auto p-4 container"}>
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-start justify-center">
+          <h1 className="text-2xl font-semibold tracking-tight" style={{ fontFamily: 'var(--font-playfair-display)' }}>Dodo Payments Assigment</h1>
+
+          <p className="text-sm text-gray-700 mt-2 max-w-md text-justify">
+            Built my first UI library from scratch using Next.js + Radix UI, took almost 8 hours, learned a ton, didn’t vibe code, and shipped a working, configurable upload component with npm + demo.
+          </p>
+        </div>
+        <div className="flex flex-row gap-4 items-start justify-start mt-4 w-full">
+          <Link href="/demo" className="text-blue-800 items-start hover:underline font-semibold" style={{ fontFamily: 'var(--font-playfair-display)' }}>/demo</Link>
+          <span className="text-gray-700">|</span>
+          <Link href="https://github.com/tsahil01/ui" className="text-blue-800 items-start hover:underline font-semibold" style={{ fontFamily: 'var(--font-playfair-display)' }}>/github</Link>
+        </div>
+      </div>
     </div>
   );
 }
